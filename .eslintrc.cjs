3e4010b3381cf3b39c37035fc1ed810398623567
Marat Nikolaev <gremlen.8149860@yandex.by>
2023-10-13 23:16:59 +0300

    init: create template to start train vue

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {},
+};
